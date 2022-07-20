d833535c1dc87ab6f642f081cba29ad0b1260b9c
Parasuatene <andoooo9.y.1127@gmail.com>
2022-07-20 16:01:44 +0900

    [Add #2] プレイヤーの弾を追加

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Rigidbody2Dコンポーネントを必須にする
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Bullet : MonoBehaviour
+{
+    // 弾の移動スピード
+    public int speed = 10;
+
+    // ゲームオブジェクト生成から削除するまでの時間
+    public float lifeTime = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ローカル座標のY軸方向に移動する
+        GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+
+        // lifeTime秒後に削除
+        Destroy(this.gameObject, lifeTime);
+    }
+}
