1ad68b1a1cd4813e17b46fd9f11685de56423834
Ohshimatani Ryo <63650325+ohshimatani@users.noreply.github.com>
2022-07-21 12:03:57 +0900

    [Add #16] 新たにSceneを作成し、左上にtextを配置した。

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour {
+
+    Text scoreNumber;
+    // Start is called before the first frame update
+    void Start() {
+        scoreNumber.text = "123";
+    }
+
+    
+}
