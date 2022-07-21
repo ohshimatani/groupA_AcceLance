5e2662466c7c48a16e9f1307a64f5f778b2bd8c3
Ohshimatani Ryo <63650325+ohshimatani@users.noreply.github.com>
2022-07-21 13:24:29 +0900

    [Add #16] インクリメント関数が機能するようにした

--- Assets/Scripts/ScoreText.cs
+++ Assets/Scripts/ScoreText.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,39 @@ using UnityEngine.UI;
 
 public class ScoreText : MonoBehaviour {
 
-    Text scoreNumber;
     // Start is called before the first frame update
     void Start() {
-        scoreNumber.text = "123";
+        Text scoreNumber = GetComponent<Text>();
+        scoreNumber.text = "0";
+    }
+
+    private float timeleft;
+    // デバッグ用の処理
+    // 本番では不要
+    void Update() {
+        timeleft -= Time.deltaTime;
+        if (timeleft <= 0.0) {
+            timeleft = 1.0f;
+            IncrementScoreNumber();
+        }
+    }
+
+    /*
+     * スコア（倒した数）をインクリメント
+     * 敵が倒されたときに呼ばれる想定
+    */
+    public void IncrementScoreNumber() {
+        Text scoreNumber = GetComponent<Text>();
+        int score = 0;
+        /*scoreNubmerをintへキャスト。
+        成功すればインクリメントし、Stringへ再度キャスト*/
+        try {
+            score = Int32.Parse(scoreNumber.text);
+            score++;
+            scoreNumber.text = score.ToString();
+        } catch(FormatException) {
+            Debug.Log("error: from string to int at ScoreText.cs/IncrementScoreNumber");
+        }
     }
 
     
