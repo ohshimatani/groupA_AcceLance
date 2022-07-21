4b1efe309974893d4250ff1d4a3cfc44a6a7464d
Ohshimatani Ryo <63650325+ohshimatani@users.noreply.github.com>
2022-07-21 14:59:09 +0900

    [Add #10] プレイヤーのHPバーを実装

---
+++ Assets/Scripts/PlayerHPBar.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHPBar : MonoBehaviour {
+
+    // フィールド
+    private int maxHp = 100;
+    public Slider playerHpBar;
+
+    /// <summary>
+    /// HPのmaxを設定＆現在のHPを満タンに
+    /// </summary>
+    void Start() {
+        playerHpBar = GetComponent<Slider>();
+        playerHpBar.maxValue = maxHp;
+        playerHpBar.value = maxHp;
+    }
+
+    /// <summary>
+    /// 一秒毎に10ダメージ受ける処理
+    /// （デバッグ用の処理。本番では不要）
+    /// </summary>
+    private float timeleft;
+    void Update() {
+        timeleft -= Time.deltaTime;
+        if (timeleft <= 0.0) {
+            timeleft = 1.0f;
+            playerDamaged(10);
+        }
+    }
+
+    /// <summary>
+    /// プレイヤーがダメージを受けた時の処理
+    /// 
+    /// </summary>
+    /// <param name="damagePoint"></param>
+    public void playerDamaged(int damagePoint) {
+        if (playerHpBar.value <= damagePoint) {
+            playerHpBar.value = 0;
+            // ゲームオーバー処理
+
+
+        } else {
+            playerHpBar.value -= damagePoint;
+        }
+        
+    }
+}
