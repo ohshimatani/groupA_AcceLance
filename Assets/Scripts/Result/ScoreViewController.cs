83ca824ab86fb39f8bb6f2aedb2855f4b9df7d71
Ohshimatani Ryo <63650325+ohshimatani@users.noreply.github.com>
2022-07-25 17:03:48 +0900

    [Add #50]UIの完成

--- Assets/Scripts/Result/ScoreViewController.cs
+++ Assets/Scripts/Result/ScoreViewController.cs
@@ -17,6 +17,7 @@ public class ScoreViewController : MonoBehaviour {
         ReflectThisScore();
         ReflectBestScore();
 
+        // ベストスコアかどうか判定し、
         if (isBestScore()) {
             bestScoreNotation.SetActive(true);
         } else {
@@ -30,7 +31,7 @@ public class ScoreViewController : MonoBehaviour {
     /// </summary>
     private void ReflectThisScore() {
         // "thisScore"キーを参照。キー登録がなければデフォルトで"0"を取得
-        string thisScore = PlayerPrefs.GetString("thisScore", "22");
+        string thisScore = PlayerPrefs.GetString("thisScore", "0");
 
         // Textに反映
         thisScoreText.text = thisScore;
@@ -41,9 +42,25 @@ public class ScoreViewController : MonoBehaviour {
     /// </summary>
     private void ReflectBestScore() {
         // "bestScore"キーを参照。キー登録がなければデフォルトで"0"を取得
-        string bestScore = PlayerPrefs.GetString("bestScore", "111");
+        string bestScore = PlayerPrefs.GetString("bestScore", "0");
 
         // Textに反映
         bestScoreText.text = bestScore;
     }
+
+    /// <summary>
+    /// 今回のスコアがベストスコアかどうかを判定
+    /// </summary>
+    /// <returns></returns>
+    private bool isBestScore() {
+        int thisScore = int.Parse(thisScoreText.text);
+        int bestScore = int.Parse(bestScoreText.text);
+
+        if (thisScore >= bestScore) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
