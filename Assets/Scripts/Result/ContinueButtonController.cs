d4fad3e578ffc92337ea690d0cac396340e930e9
Ohshimatani Ryo <63650325+ohshimatani@users.noreply.github.com>
2022-07-25 14:10:15 +0900

    [Add #53]リザルト画面にボタンを二つ作成

---
+++ Assets/Scripts/Result/ContinueButtonController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContinueButtonController : MonoBehaviour {
+
+    /// <summary>
+    /// Gameシーンに遷移させる関数(Continueボタン押下時に発動)
+    /// </summary>
+    public void SwitchGameScene() {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+}
