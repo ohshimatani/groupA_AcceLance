61d209a5c72eb51d14dcb2c8e9f9a90f2f37723a
Ohshimatani Ryo <63650325+ohshimatani@users.noreply.github.com>
2022-07-26 15:57:06 +0900

    [Add #43]ScoreManagerクラスを作成。スコアとランクの判断などを担当させる。

---
+++ Assets/Scripts/Result/PlanetImageController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanetImageController : MonoBehaviour {
+
+    // Resourcesファイル内のPlanet Image
+    private Sprite planetSprite;
+
+    // 枠内のImage
+    private Image planetImage;
+
+    // 「〇〇クラス！」のText
+    private Text planetNameText;
+
+    // ランクを表す数字
+    private int rank;
+
+    // 今回のスコア
+    private int thisScore;
+
+
+    void Start() {
+        // 各種コンポーネントを取得
+        planetImage = GetComponent<Image>();
+        planetNameText = GameObject.Find("PlanetNameText").GetComponent<Text>();
+
+        // スコアを取得
+        PlayerPrefs.SetString("thisScore", "2");
+        thisScore = int.Parse(PlayerPrefs.GetString("thisScore", "0"));
+
+        // ランクを判断
+        ScoreManager scoreManager = new ScoreManager();
+        rank = scoreManager.judgeRank(thisScore);
+        Debug.Log(rank);
+
+        // ランクから惑星の名前（英語・日本語）をそれぞれ得る
+        string planetName = scoreManager.getRankName(rank);
+        string planetNameJ = scoreManager.getRankNameJ(rank);
+
+        // Resoucesファイルからスコアに応じた画像を取得
+        planetSprite = Resources.Load<Sprite>("Planets/" + planetName);
+
+        // Textに反映
+        planetNameText.text = planetNameJ + "までとうたつ！";
+
+        // 画像を反映
+        planetImage.sprite = planetSprite;
+    }
+
+
+
+}
---
+++ Assets/Scripts/Result/ScoreManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+    // 各惑星クラスの閾値（i番目の要素がランクiの最大値）
+    private int[] planetThresholds = { 3, 5, 10, 12, 15, 20, 30 };
+
+    // 各惑星クラスの名前
+    private string[] planetNames = { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" };
+
+    // 各惑星クラスの名前（日本語名）
+    private string[] planetNamesJ = { "水星", "金星", "地球", "火星", "木星", "土星", "天王星", "海王星" };
+
+    // 惑星の配列たちのインデックスを管理する変数
+    private int planetIndex;
+
+
+    /// <summary>
+    /// 今回のスコアから、ランク(0～7)を判断し返却する関数
+    /// </summary>
+    /// <param name="thisScore">今回のスコア</param>
+    /// <returns>ランクを表す数字（0～7）</returns>
+    public int judgeRank(int thisScore) {
+        for (int i = 0; i < planetThresholds.Length; i++) {
+            // 各惑星クラスの閾値との比較を行う
+            if (thisScore <= planetThresholds[i]) {
+                return i;
+            }
+        }
+        return planetThresholds.Length;
+    }
+
+
+    /// <summary>
+    /// ランクに応じた惑星の名前を返却する関数（英語）
+    /// </summary>
+    /// <param name="rank">ランクを表す数字（0~7）</param>
+    /// <returns>惑星の名前（英語）</returns>
+    public string getRankName(int rank) {
+        return planetNames[rank];
+    }
+
+
+    /// <summary>
+    /// ランクに応じた惑星の名前を返却する関数（日本語）
+    /// </summary>
+    /// <param name="rank">ランクを表す数字（0~7）</param>
+    /// <returns>惑星の日本語名</returns>
+    public string getRankNameJ(int rank) {
+        return planetNamesJ[rank];
+    }
+
+
+}
