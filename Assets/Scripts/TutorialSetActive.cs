3647bd22c9fdfcfadee059f4aecd38b31a4eca75
Alice <araki.mmisato460@gmail.com>
2022-07-26 10:05:34 +0900

    [Update #45]レビューをもとにコードを修正

--- Assets/Scripts/TutorialSetActive.cs
+++ Assets/Scripts/TutorialSetActive.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class TutorialSetActive: MonoBehaviour
 {
-    
-    public GameObject HowToOperaion1;//XDのチュートリアル1-1-1の要素
-    public GameObject HowToOpetaion2;//XDのチュートリアル1-1-2の要素
+
+    [SerializeField] GameObject howToOperation1;//XDのチュートリアル1-1-1の要素
+    [SerializeField] GameObject howToOperation2;//XDのチュートリアル1-1-2の要素
 
     void Start()
     {
-        HowToOpetaion2.SetActive(false);
+        howToOperation2.SetActive(false);
     }
 
     public void ClickedButton()
     {
         //ボタンが押されたらオブジェクトが非表示になる
-        HowToOperaion1.SetActive(false);
+        howToOperation1.SetActive(false);
 
         //ボタンが押されたらオブジェクトが表示される
-        HowToOpetaion2.SetActive(true); 
+        howToOperation2.SetActive(true); 
     }
     
 
