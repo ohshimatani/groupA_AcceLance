1b2fcaa2dcacbb539a439c492832e7293780a010
tr-tkd <totakadaru@gmail.com>
2022-07-29 14:49:26 +0900

    [Add #64] ボタンマネージャーを追加

---
+++ Assets/Scripts/Parents/ParentsButtonManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ParentsButtonManager : MonoBehaviour
+{
+    /// <summary>
+    /// —V‚Ñ•ûƒ{ƒ^ƒ“
+    /// </summary>
+    public void OnClickTitleButton()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
