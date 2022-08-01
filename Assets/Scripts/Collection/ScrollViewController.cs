2b717f3a96413cb689ba9eed0d088c2b05814dcb
Ohshimatani Ryo <63650325+ohshimatani@users.noreply.github.com>
2022-08-01 14:54:35 +0900

    [Add # 85]Jsonファイルを書き換えて、正しく動作することを確認。

--- Assets/Scripts/Collection/ScrollViewController.cs
+++ Assets/Scripts/Collection/ScrollViewController.cs
@@ -9,7 +9,10 @@ public class ScrollViewController : MonoBehaviour
     [SerializeField] RectTransform contentRectTransform;
 
     // KanjiCellのPrefabを設定
-    [SerializeField] GameObject buttonCell;
+    [SerializeField] GameObject kanjiCellPrefab;
+
+    // SecretCellのPrefabを設定
+    [SerializeField] GameObject secretCellPrefab;
 
     /// <summary>
     /// 漢字データをJSONファイルから取得し、それぞれボタンのTextに反映する。
@@ -18,19 +21,33 @@ public class ScrollViewController : MonoBehaviour
     {
         // 漢字データをJSONファイルから取得
         JsonManager jsonManager = new JsonManager();
+        Debug.Log(StageMode.ALL);
         KanjiInfo[] kanjiInfo = jsonManager.GetKanjiInfoByStageMode(StageMode.ALL);
 
+        Debug.Log(kanjiInfo.Length);
+        // 引数に渡した漢字の情報をもとに、漢字のセル or ハテナのセルを配置
         DisposeKanjiCell(kanjiInfo);
     }
 
+    /// <summary>
+    /// 引数の情報をもとに、漢字のセル or ハテナのセルを配置する。
+    /// </summary>
+    /// <param name="kanjiInfo"></param>
     private void DisposeKanjiCell(KanjiInfo[] kanjiInfo)
     {
         // 取得した漢字の数だけKanjiCellを生成し、ScrollView内にGrid配置
         for (int i = 0; i < kanjiInfo.Length; i++)
         {
-            GameObject kanjiCell = Instantiate(buttonCell, contentRectTransform);
-            kanjiCell.GetComponentInChildren<Text>().text = kanjiInfo[i].kanji;
-            
+            if (kanjiInfo[i].defeat_count >= 1)
+            {
+                // 倒された数が１より大きいとき、その漢字Cellを表示
+                GameObject kanjiCell = Instantiate(kanjiCellPrefab, contentRectTransform);
+                kanjiCell.GetComponentInChildren<Text>().text = kanjiInfo[i].kanji;
+            } else
+            {
+                // 倒されていない漢字のとき、ハテナを表示
+                Instantiate(secretCellPrefab, contentRectTransform);
+            }
         }
     }
 }
