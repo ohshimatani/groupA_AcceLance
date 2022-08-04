f72a1f2a87e5d8f58349d8bf93966629609eabb8
tr-tkd <totakadaru@gmail.com>
2022-08-04 11:19:38 +0900

    [Update #118] DestroyAreaに名前を変更

--- Assets/Scripts/DestroyArea.cs
+++ Assets/Scripts/DestroyArea.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyAreaManager : MonoBehaviour
+public class DestroyArea : MonoBehaviour
 {
-
-    // トリガーから出た時の処理
-    // 衝突した相手の Collider2D コンポーネントを引数 c に格納
+    /// <summary>
+    /// トリガーから出た時の処理
+    /// 衝突した相手の Collider2D コンポーネントを引数 c に格納
+    /// </summary>
+    /// <param name="c"></param>
     void OnTriggerExit2D(Collider2D c)
     {
         // 引数 c に格納されたコンポーネント（オブジェクト）を削除
