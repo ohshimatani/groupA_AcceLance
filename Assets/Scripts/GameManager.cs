773d7ba5b1a96621f09ee98663597b9eac1e4859
Parasuatene <andoooo9.y.1127@gmail.com>
2022-08-04 19:25:20 +0900

    [Update #143] ランク（惑星）に関する情報をGameManagerで管理するように変更

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,15 +6,37 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     // 現在のスコア
-    public int score { get; private set; } = 0;
+    public static int score { get; private set; }
 
     // スコアを表示するテキストとそのコンポーネント
     [SerializeField] GameObject scoreTextObject;
     private ScoreText scoreText;
 
+    // 惑星の配列
+    private Planet[] planets;
+
+    // 現在のランク（惑星）を記録
+    public static Planet currentPlanet { get; private set; }
+
     private void Start()
     {
+        score = 0;
+
         scoreText = scoreTextObject.GetComponent<ScoreText>();
+
+        planets = new Planet[] {
+            new Planet("Mercury", "水星", 0, -5.4f)
+            , new Planet("Venus", "金星", 4, -4.33f)
+            , new Planet("Earth", "地球", 8, -3f)
+            , new Planet("Mars", "火星", 11, -1.7f)
+            , new Planet("Jupiter", "木星", 16, 0f)
+            , new Planet("Saturn", "土星", 21, 2.73f)
+            , new Planet("Uranus", "天王星", 31, 4.87f)
+            , new Planet("Neptune", "海王星", 51, 6.35f)
+        };
+
+        // 現在のランクの初期値は水星とする
+        currentPlanet = planets[0];
     }
 
     /// <summary>
@@ -24,6 +46,9 @@ public class GameManager : MonoBehaviour
     {
         score++;
         scoreText.SetText(score.ToString());
+
+        // 必要に応じてランクを更新する
+        UpdateCurrentPlanet();
     }
 
     /// <summary>
@@ -36,5 +61,25 @@ public class GameManager : MonoBehaviour
 
         // リザルト画面に遷移
         SceneManager.LoadScene("Result");
+    }
+
+    /// <summary>
+    /// スコアが加算された際に、条件に応じてランクを更新する
+    /// </summary>
+    private void UpdateCurrentPlanet()
+    {
+        // 最大スコアよりも上である場合は処理を行わない
+        if (score > planets[planets.Length-1].scoreMin)
+        {
+            return;
+        }
+
+        foreach (Planet planet in planets)
+        {
+            if (score == planet.scoreMin)
+            {
+                currentPlanet = planet;
+            }
+        }
     }
 }
