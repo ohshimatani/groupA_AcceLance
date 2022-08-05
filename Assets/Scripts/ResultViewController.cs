453aa8c9f285de89c6cb882df8519ad0cf747de3
Parasuatene <andoooo9.y.1127@gmail.com>
2022-08-05 10:37:39 +0900

    [Update #143] ScoreViewの処理をResultViewに統合

--- Assets/Scripts/ResultViewController.cs
+++ Assets/Scripts/ResultViewController.cs
@@ -11,6 +11,42 @@ public class ResultViewController : MonoBehaviour
     // 「〇〇 まで とうたつ！」のText
     [SerializeField] Text planetNameText;
 
+    // たおした数
+    [SerializeField] Text thisScoreText;
+
+    // 最高スコア
+    [SerializeField] Text bestScoreText;
+
+    // 「さい高記ろく！」の表示Text
+    [SerializeField] GameObject bestScoreNotation;
+
+    /// <summary>
+    /// 今回のスコアをテキストに表示
+    /// </summary>
+    /// <param name="thisScore"></param>
+    public void SetThisScoreText(string thisScore)
+    {
+        thisScoreText.text = thisScore;
+    }
+
+    /// <summary>
+    /// 今までの最高スコアをテキストに表示
+    /// </summary>
+    /// <param name="bestScore"></param>
+    public void SetBestScoreText(string bestScore)
+    {
+        bestScoreText.text = bestScore;
+    }
+
+    /// <summary>
+    /// 「さい高記ろく！」を表示するか否か
+    /// </summary>
+    /// <param name="isActive"></param>
+    public void SetActiveBestScoreNotation(bool isActive)
+    {
+        bestScoreNotation.SetActive(isActive);
+    }
+
     /// <summary>
     /// ImageとTextを反映させる
     /// </summary>
