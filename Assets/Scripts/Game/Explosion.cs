86d3049d24639262f7a199ee4cd8414391d6ea94
Parasuatene <andoooo9.y.1127@gmail.com>
2022-08-09 00:13:23 +0900

    [Update #171] Explosionの音声処理を更新

--- Assets/Scripts/Game/Explosion.cs
+++ Assets/Scripts/Game/Explosion.cs
@@ -3,17 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Explosion : MonoBehaviour
-{
-    AudioSource audioSource;
-    public AudioClip boomSE;
-    
+{   
     /// <summary>
     /// アニメーションの終了処理
     /// </summary>
     void OnAnimationFinish()
     {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.PlayOneShot(boomSE);
+        // オブジェクトの削除
         Destroy(gameObject);
     }
 }
