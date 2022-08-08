dc2c35fceb570520386e2cc1a9d21d173a6ee620
tr-tkd <totakadaru@gmail.com>
2022-08-08 16:35:57 +0900

    [Add #171] 爆発SEを実装

--- Assets/Scripts/Game/Explosion.cs
+++ Assets/Scripts/Game/Explosion.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
+    AudioSource audioSource;
+    public AudioClip boomSE;
+    
     /// <summary>
     /// アニメーションの終了処理
     /// </summary>
     void OnAnimationFinish()
     {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(boomSE);
         Destroy(gameObject);
     }
 }
