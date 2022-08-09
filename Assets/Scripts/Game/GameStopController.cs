9d39905811cef5534ebf6bb16901608b7bffcb94
Parasuatene <andoooo9.y.1127@gmail.com>
2022-08-09 11:59:46 +0900

    [Add #169] 一時停止機能を実装

--- Assets/Scripts/Game/GameStopController.cs
+++ Assets/Scripts/Game/GameStopController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class GameStopButtonController : MonoBehaviour
+public class GameStopController : MonoBehaviour
 {
     // 表示させるポップアップオブジェクト
     [SerializeField] GameObject popUpObject;
